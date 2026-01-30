3dff9985d1c190547bce97c40eb606423fb69b4a
allwyn samuvel <allwynsamuvel90@gmail.com>
2026-01-30 21:42:18 +0530

    add express-mongo-sanitize and comments

--- index.js
+++ index.js
@@ -3,31 +3,37 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize'); /* Sanitization */
 
-dotenv.config({ quiet: true }); /* quiet is used to silence runtime logging message */
+dotenv.config({ quiet: true }); /* Quiet is used to silence runtime logging message */
 
 console.log(`\n=== Environment: ${process.env.NODE_ENV} ===`);
 
 const app = express();
 
-app.use(helmet());
-app.use(morgan(':method :url :date :remote-addr :status :response-time'));
+app.use(morgan(':method :url :date :remote-addr :status :response-time ms'));
+
+// Security Middleware
+app.use(helmet()); /* Secure Headers */
 app.use(cors({
   origin: '*',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
-}))
+}));
 
-// parses incoming requests
-app.use(express.json({ limit: '100kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+// Parses incoming requests
+app.use(express.json({ limit: '100kb' })); /* Limit body size to prevent DoS */
+app.use(express.urlencoded({ extended: true, limit: '10kb' })); /* Parse URL-encoded data (from HTML forms) */
 
-// handle invalid routes
+// Prevents NoSQL Injection (removes $ and . from user input)
+app.use(mongoSanitize());
+
+// Handle invalid routes
 app.use('/', (_req, res) => {
   return res.status(404).send('Route not found');
 });
 
-// global error handling
+// Global error handling
 app.use((err, _req, res) => {
   return res.status(err.status || 500).send(err.message || 'Internal Server Error');
 });
